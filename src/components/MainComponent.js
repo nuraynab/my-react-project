2ee47dd4ffef2dc425298f118a1a81379a42e750
nuraynab <nuraynab@gmail.com>
2020-06-25 13:53:45 +0600

    assignment 2: about us page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import {DISHES} from "../shared/dishes";
 import {LEADERS} from "../shared/leaders";
 import {PROMOTIONS} from "../shared/promotions";
@@ -45,6 +46,7 @@ class Main extends React.Component{
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact}/>
+                    <Route path='/aboutus' component={() => <About leaders={this.state.leaders}/> } />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
