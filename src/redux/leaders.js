2646deeb26dd49cd4d8fb47dbee9c2651e6cfd37
nuraynab <nuraynab@gmail.com>
2020-07-07 14:31:18 +0600

    assignment 4, fetching leaders, posting feedback, animation

--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,7 +1,16 @@
-import {LEADERS} from "../shared/leaders";
+import * as ActionTypes from './ActionTypes';
 
-export const Leaders = (state = LEADERS, action) => {
+export const Leaders = (state = {
+    isLoading: true,
+    errMess: null,
+    leaders: []}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_LEADERS:
+            return {...state, isLoading: false, leaders: action.payload, errMess: null}
+        case ActionTypes.LEADERS_LOADING:
+            return {...state, isLoading: true, leaders: [], errMess: null}
+        case ActionTypes.LEADERS_FAILED:
+            return {...state, isLoading: false, leaders: [], errMess: action.payload}
         default:
             return state;
     }
